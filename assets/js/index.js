10ea9213e9d89023b633476e9e70daa3bd1e3fe0
Paul Guerra <paulguerra@Pauls-Air.home>
2021-11-28 22:23:57 -0600

    javascript edits

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,10 @@
-var startButton = document.getElementById('start-btn');
-var nextButton = document.getElementById('next-btn');
-var questionContainer = document.getElementById('question-container');
+// initial variables for script
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainer = document.getElementById("question-container");
 var questionElement = document.getElementById('question');
-var answerButtons = document.getElementById('answer-btns');
-
-let shuffledQuestions, currentQuestion;
-
-startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click', () => {
-    currentQuestion++;
-    setNextQuestion();
-})
-
-function startGame() {
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestion = 0;
-    questionContainer.classList.remove('hide');
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestion]);
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        var button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-
-        button.addEventListener('click', selectAnswer);
-        answerButtons.appendChild(button);
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add('hide');
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    };
-}
-
-function selectAnswer(e) {
-    var selectedButton = e.target;
-    var correct = selectedButton.dataset.correct;
-    setStatusClass(document.body, correct);
-    Array.from(answerButtons.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct);
-    });
-    if (shuffledQuestions.length > currentQuestion + 1) {
-        nextButton.classList.remove('hide');
-    } else {
-        startButton.innerText = 'Restart';
-        startButton.classList.remove('hide');
-    };
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-        element.classList.add('correct');
-    } else {
-        element.classList.add('wrong');
-    };
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct');
-    element.classList.remove('wrong');
-}
-
+var answerButtons = document.getElementById("answer-btns");
+// questions and answers for quiz
 var questions = [
     {
         question: "The condition in an if/else statement is enclosed with _______.",
@@ -114,4 +42,77 @@ var questions = [
             { text: "4. console.log", correct: true }
         ]
     }
-]
+];
+// variables for every time the user takes this quiz, the questions shuffle.
+let shuffledQuestions, currentQuestion;
+
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+    currentQuestion++;
+    setNextQuestion();
+})
+// starting the quiz
+function startQuiz() {
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestion = 0;
+    questionContainer.classList.remove("hide");
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestion]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+
+        button.addEventListener("click", selectAnswer);
+        answerButtons.appendChild(button);
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add("hide");
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    };
+}
+// selecting answers for the questions
+function selectAnswer(e) {
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtons.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestion + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        startButton.innerText = "Restart";
+        startButton.classList.remove("hide");
+    };
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("incorrect");
+    };
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("incorrect");
+};
